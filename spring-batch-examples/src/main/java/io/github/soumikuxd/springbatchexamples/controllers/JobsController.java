1a196e35c605ccc6b7327d24204eaff746a33677
Soumik Das <soumik.uxd @gmail.com>
2023-06-05 00:22:29 +0200

    Infoservice cleanup

--- spring-batch-examples/src/main/java/io/github/soumikuxd/springbatchexamples/controllers/JobsController.java
+++ spring-batch-examples/src/main/java/io/github/soumikuxd/springbatchexamples/controllers/JobsController.java
@@ -1,13 +1,11 @@
 package io.github.soumikuxd.springbatchexamples.controllers;
 
 import io.github.soumikuxd.springbatchexamples.exceptions.ResourceNotFoundException;
-import io.github.soumikuxd.springbatchexamples.models.JobInfo;
 import io.github.soumikuxd.springbatchexamples.services.JobHandlerService;
-import io.github.soumikuxd.springbatchexamples.services.JobInfoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,23 +14,15 @@ import java.util.List;
 public class JobsController {
     private static final Logger logger = LoggerFactory.getLogger(JobsController.class);
     @Autowired
-    private JobInfoService jobInfoService;
-
-    @Autowired
     private JobHandlerService jobHandlerService;
 
-    @GetMapping("/jobs")
-    public List<JobInfo> getAllJobs() {
-        return jobInfoService.getAllJobs();
-    }
-
     @PostMapping("/job/{jobName}")
-    public ResponseEntity<JobInfo> submit(@PathVariable(value = "jobName") String jobName) {//@RequestBody JobExecutionParameters jobExecParameters) {
+    @ResponseStatus(code = HttpStatus.OK)
+    public void submit(@PathVariable(value = "jobName") String jobName) {
         try {
             this.jobHandlerService.submit(jobName);
-        } catch (ResourceNotFoundException e) {
-            //logger.error("Error submitting job: " + jobName);
+        } catch (ResourceNotFoundException ignored) {
+
         }
-        return ResponseEntity.ok().body(new JobInfo(jobName));
     }
 }
