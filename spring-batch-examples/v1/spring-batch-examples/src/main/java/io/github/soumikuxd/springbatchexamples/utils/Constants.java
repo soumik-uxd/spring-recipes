b3350026b51d1cad2d36a066a35f2d66d324fcc1
Soumik Das <soumik.uxd @gmail.com>
2023-06-14 23:38:54 +0200

    Redundant dir cleanup

--- spring-batch-examples/v1/spring-batch-examples/src/main/java/io/github/soumikuxd/springbatchexamples/utils/Constants.java
+++
@@ -1,6 +0,0 @@
-package io.github.soumikuxd.springbatchexamples.utils;
-
-public class Constants {
-    public static final String FILE_NAME_CONTEXT_KEY = "fileName";
-    public static final long MAX_ALLOWED_BAD_RECORDS = 2;
-}
