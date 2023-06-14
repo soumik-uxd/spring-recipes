6a76d71e81c9952a9dc0fc0090b513f1f3c97a9f
Soumik Das <soumik.uxd @gmail.com>
2023-06-14 23:38:54 +0200

    Redundant dir cleanup

--- spring-batch-examples/v1/spring-batch-examples/src/main/java/io/github/soumikuxd/springbatchexamples/repositories/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.soumikuxd.springbatchexamples.repositories;
-
-import io.github.soumikuxd.springbatchexamples.models.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, String> {
-}
