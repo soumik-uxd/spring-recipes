922652243a5514b7f77f50d393da35832b7ae900
Soumik Das <soumik.uxd @gmail.com>
2023-07-01 01:41:33 +0200

    Init for DBExtractor

---
+++ spring-batch-examples/v2/dbextractor/src/main/java/io/github/soumikuxd/dbextractor/DbExtractorApplication.java
@@ -0,0 +1,13 @@
+package io.github.soumikuxd.dbextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbExtractorApplication.class, args);
+	}
+
+}
---
+++ spring-batch-examples/v2/dbextractor/src/test/java/io/github/soumikuxd/dbextractor/DbextractorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.soumikuxd.dbextractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbextractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
