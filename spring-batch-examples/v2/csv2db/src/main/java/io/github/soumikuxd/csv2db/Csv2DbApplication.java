14e16ed25ae321a638bfc9b3924775e6a0318408
Soumik Das <soumik.uxd @gmail.com>
2023-07-01 01:46:34 +0200

    Removed redundant imports

--- spring-batch-examples/v2/csv2db/src/main/java/io/github/soumikuxd/csv2db/Csv2DbApplication.java
+++ spring-batch-examples/v2/csv2db/src/main/java/io/github/soumikuxd/csv2db/Csv2DbApplication.java
@@ -1,9 +1,7 @@
 package io.github.soumikuxd.csv2db;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class Csv2DbApplication {
