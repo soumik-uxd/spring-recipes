61ebcb21e8b42f0071e8d216be1df63ead012a84
Soumik Das <soumik.uxd @gmail.com>
2023-07-07 22:28:28 +0200

    Init for TaskExec

---
+++ spring-batch-examples/v2/taskexec/src/main/java/io/github/soumikuxd/taskexec/TaskexecApplication.java
@@ -0,0 +1,13 @@
+package io.github.soumikuxd.taskexec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskexecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskexecApplication.class, args);
+	}
+
+}
