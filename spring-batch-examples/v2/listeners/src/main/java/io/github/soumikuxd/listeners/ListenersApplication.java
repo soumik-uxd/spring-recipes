8619ce45504585ac132641983b750b147701ad8d
Soumik Das <soumik.uxd @gmail.com>
2023-07-09 02:41:41 +0200

    Init for Listener

---
+++ spring-batch-examples/v2/listeners/src/main/java/io/github/soumikuxd/listeners/ListenersApplication.java
@@ -0,0 +1,13 @@
+package io.github.soumikuxd.listeners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListenersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListenersApplication.class, args);
+	}
+
+}
