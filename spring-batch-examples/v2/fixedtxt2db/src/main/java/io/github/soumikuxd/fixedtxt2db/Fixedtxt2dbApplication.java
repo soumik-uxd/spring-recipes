8f1ea5de0b8cea8c24c5e44e1551c02bc6ce0d25
Soumik Das <soumik.uxd @gmail.com>
2023-07-10 00:59:15 +0200

    Init for FixedTxt2DB

---
+++ spring-batch-examples/v2/fixedtxt2db/src/main/java/io/github/soumikuxd/fixedtxt2db/Fixedtxt2dbApplication.java
@@ -0,0 +1,13 @@
+package io.github.soumikuxd.fixedtxt2db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fixedtxt2dbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fixedtxt2dbApplication.class, args);
+	}
+
+}
