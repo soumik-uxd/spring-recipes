0b45e116b59812c2f1dabc903311b7994f7519f7
Soumik Das <soumik.uxd @gmail.com>
2023-07-14 03:07:04 +0200

    Init for EmailSender

---
+++ spring-batch-examples/v2/emailsender/src/main/java/io/github/soumikuxd/emailsender/processors/EmployeeProcessor.java
@@ -0,0 +1,14 @@
+package io.github.soumikuxd.emailsender.processors;
+
+import io.github.soumikuxd.emailsender.models.Employee;
+import jakarta.annotation.Nonnull;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmployeeProcessor implements ItemProcessor<Employee, Employee> {
+    @Override
+    public Employee process(@Nonnull Employee employee) throws Exception {
+        return employee;
+    }
+}
